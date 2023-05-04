ce4c1960052538a6557c59363cd1f9dde4e22851
talkifier1 <ali.elhelali12@gmail.com>
2023-05-04 17:21:54 -0400

    working website

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from survey.routes import survey
 
-app = Flask(__name__)
-app.register_blueprint(survey)
-
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(survey)
+    return app
+app = create_app()
 app.run(debug=True, port="8000")
--- survey/routes.py
+++ survey/routes.py
@@ -1,8 +1,10 @@
-from flask import jsonify, Blueprint,request
+from flask import jsonify, Blueprint,request,render_template,send_from_directory
 from decorators import mongo
-survey = Blueprint("users", __name__, template_folder="templates")
+import os
 
-@survey.route('/update_survey',methods=['POST'])
+survey = Blueprint("survey", __name__, template_folder="templates",static_folder="build/static")
+
+@survey.route('/update_survey',methods=['POST'],)
 @mongo
 def check_server(db):
     try:
@@ -16,3 +18,15 @@ def check_server(db):
     except Exception as E:
         print(E)
         return jsonify({'success': False})
+    
+@survey.route('/seller_intake_survey')
+def seller_intake_survey():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(current_dir, "build")
+    return send_from_directory(build_dir, "index.html")
+
+@survey.route('/seller_intake_survey_results')
+@mongo
+def seller_intake_survey_results(db):
+    surveys = list(db['surveys'].find())
+    return render_template("seller_intake_survey_results.html",surveys=surveys)
